a55c4c99e7b16c802da85c3b09c3a7b37711d0a0
Ben Bailey <bennettbailey@gmail.com>
2014-01-10 09:46:27 -0600

    added grid to brb page

--- app.js
+++ app.js
@@ -112,31 +112,33 @@ app.post('/brb/email', function(req, res){
 
 app.post('/brb/sms', function(req, res){
 	var sms = req.param('sms', null);
-	for(var i = 0; i < req.user.phone.length; i++){
-    var currentPhone = req.user.phone[i];
-    var util = require("util");
-  	var Client = require('telapi').client;
+	for(var i = 0; i < req.user.contacts.length; i++){
+    if(req.user.contacts[i].phone != null){
+      var currentPhone = req.user.contacts[i].phone;
+      var util = require("util");
+    	var Client = require('telapi').client;
 
-  	var client = new Client(process.env.TELAPI_SID, process.env.TELAPI_TOKEN2); 
+    	var client = new Client(process.env.TELAPI_SID, process.env.TELAPI_TOKEN2); 
 
-  	var options = {
-  	    "From": process.env.TELAPI_NUMBER,
-  	    "To": currentPhone,
-  	    "Body": "Emergency! " + req.user.name + " needs your help. Text them to let them know you\'re there for them. \n- Defcon One \n(http://www.defconone.us)"
-  	};
+    	var options = {
+    	    "From": process.env.TELAPI_NUMBER,
+    	    "To": currentPhone,
+    	    "Body": "Emergency! " + req.user.name + " needs your help. Text them to let them know you\'re there for them. \n- Defcon One \n(http://www.defconone.us)"
+    	};
 
 
-  	client.create("sms_messages", options, function (response) {
-  	        util.log("SmsMessage SID: " +  response.sid);
-            req.flash('success', 'Text messages sent successfully.');
-            res.redirect('back');
-  	    },
-  	    function (error) {
-  	        util.log("Error: " + error);
-            req.flash('error', 'An error occured while sending text messages.');
-            res.redirect('back');
-  	    }
-  	);
+    	client.create("sms_messages", options, function (response) {
+    	        util.log("SmsMessage SID: " +  response.sid);
+              req.flash('success', 'Text messages sent successfully.');
+              res.redirect('back');
+    	    },
+    	    function (error) {
+    	        util.log("Error: " + error);
+              req.flash('error', 'An error occured while sending text messages.');
+              res.redirect('back');
+    	    }
+    	);
+    }
   }
 });
 
