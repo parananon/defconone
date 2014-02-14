fa701a8f470c2ffe53e97ab5b597aa5e0f4732ca
Ben Bailey <bennettbailey@gmail.com>
2014-02-14 11:39:54 -0600

    use jslint to clean up routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,39 @@
+/*jslint node: true */
+"use strict";
 
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: "Home", messageE: req.flash('error'), messageI: req.flash('info'), messageS: req.flash('success') });
+exports.index = function (req, res) {
+    res.render('index', { title: "Home", messageE: req.flash('error'), messageI: req.flash('info'), messageS: req.flash('success') });
 };
 
-exports.about = function(req, res){
+exports.about = function (req, res) {
     res.render('about', { title: 'About' });
 };
 
-exports.brb = function(req, res){
+exports.brb = function (req, res) {
 	res.render('brb', { title: "Big Red Button", user: req.user, messageE: req.flash('error'), messageI: req.flash('info'), messageS: req.flash('success') });
 };
 
-exports.hotlines = function(req, res){
+exports.hotlines = function (req, res) {
 	res.render('hotlines', { title: 'Hotlines' });
 };
 
-exports.signup = function(req, res){
+exports.signup = function (req, res) {
 	res.render('signup', { title: "Sign Up", messageE: req.flash('error'), messageI: req.flash('info'), messageS: req.flash('success') });
-}
+};
 
-exports.signin = function(req, res){
+exports.signin = function (req, res) {
 	res.render('signin', { title: 'Sign In', messageE: req.flash('error'), messageI: req.flash('info'), messageS: req.flash('success') });
-}
+};
 
-exports.settings = function(req, res){
+exports.settings = function (req, res) {
 	res.render('settings', { title: "Settings", user: req.user, messageE: req.flash('error'), messageI: req.flash('info'), messageS: req.flash('success') });
-}
+};
 
-exports.contacts = function(req, res){
+exports.contacts = function (req, res) {
 	res.render('contacts', { title: "Contacts", user: req.user, messageE: req.flash('error'), messageI: req.flash('info'), messageS: req.flash('success') });
-}
+};
 
-exports.logout = function(req, res){
+exports.logout = function (req, res) {
     req.logout();
     res.redirect('/');
-}
+};
