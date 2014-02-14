5fea2e3f1419d7e9dee2b8ed150f9249b0af43f2
Ben Bailey <bennettbailey@gmail.com>
2014-02-14 12:01:13 -0600

    fix final jslint stuff

--- routes/brb.js
+++ routes/brb.js
@@ -1,4 +1,5 @@
 /*jslint node: true */
+/*jslint plusplus: true */
 "use strict";
 
 var util = require("util");
@@ -9,72 +10,69 @@ var SendGrid = require('sendgrid').SendGrid;
 var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 
 exports.email = function (req, res) {
-    var suc = true, crisis = req.param('crisis', null);
-    for (var i = 0; i < req.user.contacts.length; i++){
-        if(req.user.contacts[i].email != null){
-            var addr = req.user.contacts[i].email;
-            var etxt = 'Email ' + req.user.name + config.email;
-            if(crisis=="true"){
+    var suc = true, crisis = req.param('crisis', null), i = 0, addr, etxt;
+    for (i; i < req.user.contacts.length; i++) {
+        if (req.user.contacts[i].email !== null) {
+            addr = req.user.contacts[i].email;
+            etxt = 'Email ' + req.user.name + config.email;
+            if (crisis === "true") {
                 etxt = config.cemail + req.user.name + ' (via the Defcon One app)';
             }
-        sendgrid.send({
-            to: addr,
-            fromname: "Defcon One",
-            from: process.env.SENDGRID_USERNAME,
-            subject: 'Emergency! ' + req.user.name + ' needs your help',
-            text: etxt}, function(success, message) {
+            sendgrid.send({
+                to: addr,
+                fromname: "Defcon One",
+                from: process.env.SENDGRID_USERNAME,
+                subject: 'Emergency! ' + req.user.name + ' needs your help',
+                text: etxt
+            }, function (success, message) {
                 if (!success) {
                     console.log(message);
                     suc = false;
-                }
-                else {
+                } else {
                     console.log(message);
                     suc = true;
                 }
             });
         }
     }
-    if(suc == true) {
+    if (suc === true) {
         req.flash('success', 'Emails sent successfully.');
         res.redirect('back');
-    }
-    else {
+    } else {
         req.flash('error', 'An error occured while sending emails.');
         res.redirect('back');
     }
 };
 
-exports.sms = function(req, res){
-    var crisis = req.param('crisis', null);
-    var suc = true;
-    for(var i = 0; i < req.user.contacts.length; i++){
-        if(req.user.contacts[i].phone != null){
-            var currentPhone = req.user.contacts[i].phone;
-            var options = {
+exports.sms = function (req, res) {
+    var crisis = req.param('crisis', null), suc = true, i = 0, currentPhone, options;
+    for (i; i < req.user.contacts.length; i++) {
+        if (req.user.contacts[i].phone !== null) {
+            currentPhone = req.user.contacts[i].phone;
+            options = {
                 From: process.env.TELAPI_NUMBER,
                 To: currentPhone,
                 Body: req.user.name + config.sms
             };
-            if(crisis=="true"){
+            if (crisis === "true") {
                 options.Body = req.user.name + config.csms;
             }
             client.create("sms_messages", options, function (response) {
                 util.log("SmsMessage SID: " +  response.sid);
                 suc = true;
             },
-            function (error) {
-    	       util.log("Error: " + error);
-                suc = false;
-    	    }
-            );
+                function (error) {
+                    util.log("Error: " + error);
+                    suc = false;
+                }
+                );
         }
     }
-    if(suc == true) {
+    if (suc === true) {
         req.flash('success', 'Text messages sent successfully.');
         res.redirect('back');
-    }
-    else {
+    } else {
         req.flash('error', 'An error occured while sending text messages.');
         res.redirect('back');
     }
-}
+};
